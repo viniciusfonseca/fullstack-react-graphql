8f3fc0956a73ecd10154f2b288f4affee2f27430
Vinicius Alves Fonseca <vfonseca1618@gmail.com>
2020-07-03 11:17:09 -0300

    add vercel support

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { createSchema } from "./graphql/createSchema";
 import { ApolloServer } from 'apollo-server'
 import { createApolloServer } from './graphql/createApolloServer';
 import { createContainer } from './services/createContainer';
+import * as http from 'http'
+import { EventEmitter } from 'events'
+
+let httpServer: http.Server = null
+let evt = new EventEmitter()
 
 async function main() {
 
@@ -10,9 +15,25 @@ async function main() {
 
     const server = await createApolloServer(container)
 
-    const { port } = await server.listen(process.env.PORT ?? 4000)
+    const serverInfo = await server.listen(process.env.PORT ?? 4000)
+    httpServer = serverInfo.server
+    evt.emit('ready')
 
-    console.log(`🚀 Apollo Server listening at port ${port}`)
+    console.log(`🚀 Apollo Server listening at port ${serverInfo.port}`)
 }
 
 main()
+
+module.exports = http.createServer((req, res) => {
+
+    function onServerReady() {
+        httpServer.emit('request', req, res)
+    }
+
+    if (!httpServer) {
+        evt.once('ready', onServerReady)
+        return
+    }
+
+    onServerReady()
+})
